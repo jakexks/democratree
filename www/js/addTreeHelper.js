7951ae4f9ea32703ddc04b3852983afd38121aff
Jake <i@am.so-aweso.me>
2014-04-02 20:01:31 +0100

    Prepared add tree pages for real code

--- www/js/addTreeHelper.js
+++ www/js/addTreeHelper.js
@@ -1,4 +1,16 @@
+var newChosenTree = "";
+var newTreeLabel = "";
+var newTreeStory = "";
+var newTreeImg = null;
 
+function addTree1() {
+	newChosenTree = $("#chosenTree :radio:checked").val();
+}
+
+function addTree2() {
+	newTreeLabel = $("#tree-label").val();
+	newTreeStory = $("#tree-story").val();
+}
 
 function attachPhoto1() {
 	navigator.camera.getPicture(onAddTreeSuccess, onAddTreeFail, { quality: 50, destinationType: Camera.DestinationType.DATA_URL});
@@ -12,6 +24,7 @@ function attachPhoto2() {
 function onAddTreeSuccess(imageData) {
     var image = document.getElementById('myImage');
     image.src = "data:image/jpeg;base64," + imageData;
+    newTreeImg = imageData;
 }
 
 function onAddTreeFail(message) {
